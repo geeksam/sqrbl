67a71029cd64bc438d18dd463ff2f380e1e0e459
Sam Livingston-Gray <geeksam@gmail.com>
2009-08-07 11:56:21 -0700

    Fix requirements list in README.txt, add TODO.txt.

--- lib/sqrbl.rb
+++ lib/sqrbl.rb
@@ -6,7 +6,7 @@
 module Sqrbl
 
   # :stopdoc:
-  VERSION = '0.1.0'
+  VERSION = '0.1.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH    = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
